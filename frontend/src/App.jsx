b7f2f820c468bb9c3b1c97d98c1b209a55008731
Ankur Pandey <pankur216@gmail.com>
2024-11-17 00:58:28 -0500

    Added routes to all client side endpoints

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,26 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-function App() {
+const router = createBrowserRouter([
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+  {
+    path: "/signin",
+    element: <SignIn />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+  {
+    path: "/send",
+    element: <Send />,
+  },
+]);
 
-  return (
-    <div>
-        Hello world
-    </div>
-  )
-}
-
-export default App
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
